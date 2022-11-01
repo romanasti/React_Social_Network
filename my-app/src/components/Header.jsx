022a8683bdb4c0850278ed896289b7524c7092c7
Roman_Chub <rchub@inbox.ru>
2022-11-01 23:05:46 +0300

    app.js is changed, .jsx files is added, css-modules is passed

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png' />
+    </header>
+}
+
+export default Header;
