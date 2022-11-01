9e4ae20ac6dff6a44a32caa67a2a8691c08f9ad6
Roman_Chub <rchub@inbox.ru>
2022-11-02 01:26:55 +0300

    Added component props

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,8 +10,8 @@ const MyPosts = () => {
       <button>Add post</button>
     </div>
     <div className={s.posts}>
-      <Post />
-      <Post />
+      <Post message ='Hi, how are you?' />
+      <Post message ="It's my first post"/>
     </div>
   </div>
 }
--- my-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ my-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
   return <div className={s.item}>
     <img src='https://i.ytimg.com/vi/6I4TN185Szw/maxresdefault.jpg' />
-    post 1
+    {props.message}
     <div>
       <span>like</span>
     </div>
