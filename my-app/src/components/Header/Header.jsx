14a80b87f3447f80d7e9b9e3d82e49d7ff9b4068
Roman_Chub <rchub@inbox.ru>
2022-11-01 23:39:06 +0300

    component structure added

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Header.css'
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png' />
     </header>
 }
