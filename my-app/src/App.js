c782ec541b6628122b4bf3b52fe7a9296d665723
Roman_Chub <rchub@inbox.ru>
2022-11-04 11:16:40 +0300

    installation and operation with react-route-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,22 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div class='app-wrapper-content'>
-                <Dialogs />
-            </div>
-            {/*<Profile />*/}
-        </div>
+        <BrowserRouter>
+                <div className='app-wrapper'>
+                    <Header />
+                    <Navbar />
+                    <div class='app-wrapper-content'>
+                        <Routes>
+                        <Route exact path='/profile' element={<Profile />} />
+                        <Route exact path='/dialogs' element={<Dialogs />} />
+                        </Routes>
+                    </div>
+                </div>
+        </BrowserRouter>
     );
 }
 
