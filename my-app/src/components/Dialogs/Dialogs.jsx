6d3f60b4e05456e057b2b1af4ab91300234bacf5
Roman_Chub <rchub@inbox.ru>
2022-11-05 23:57:34 +0300

    dialog box filled and links connected

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const setActive = ({isActive}) => ({color: isActive ? 'blue' : "white"});
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1' style={setActive}>Roma</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2' style={setActive}>Ivan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3' style={setActive}>Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4' style={setActive}>Kostya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5' style={setActive}>Victor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6' style={setActive}>Alex</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/7' style={setActive}>Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
