177bb14a81450b70f1aac0d231f3384926f5e986
Roman_Chub <rchub@inbox.ru>
2022-12-05 02:15:15 +0300

    processed side Effect in the Users component

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -4,14 +4,18 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        });
 
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
+
     return (
         <div className={s.us}>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
